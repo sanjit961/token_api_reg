cfd44542f4720623f1a00217301cf1b93f38c82e
sanjit961 <sanjitlungeli241@gmail.com>
2023-01-30 19:42:16 +0530

    initial

---
+++ app.js
@@ -0,0 +1,129 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./model/user");
+const auth = require("./middleware/auth");
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.post("/register", async (req, res) => {
+  try {
+    const { first_name, last_name, email, password } = req.body;
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+    encryptedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
+app.get("/getall", auth, async (req, res) => {
+  try {
+    const data = await User.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get by ID Method
+app.get("/:id", auth, async (req, res) => {
+  try {
+    const data = await User.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+app.patch("/update/:id", auth, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+
+    const result = await User.findByIdAndUpdate(id, updatedData, options);
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+//Delete by ID Method
+app.delete("/delete/:id", auth, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await User.findByIdAndDelete(id);
+    res.send(data);
+    res.status(200).json({ message: "Deleted success" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+//Delete all
+app.delete("/deleteAll", auth, async (req, res) => {
+  try {
+    await User.deleteMany();
+    res.status(200).json({ message: "Deleted success" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
+});
+module.exports = app;
